97dbe63b423e403834cdbc001973bf3605b6d381
Maksim Majmari <maksimmajmari12@gmail.com>
2023-02-06 00:33:21 +0100

    Showing ListOfActors from Db

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        //Declare AppDbContent
+        private readonly AppDbContent _context;
+
+        public ActorsController(AppDbContent context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Actors.ToListAsync();
+
+            return View(data);
+        }
+    }
+}
